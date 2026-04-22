bff1a6dd545e2389adbafeb62664b4e16840fc68
Juan Vicente Quiles Puchalt <jvquiles@gmail.com>
2026-04-22 11:02:03 +0200

    test: add failing test for posting a rental with invalid payload

---
+++ test/infrastructure/GtMotive.Estimate.Microservice.InfrastructureTests/Specs/RentalsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using GtMotive.Estimate.Microservice.InfrastructureTests.Infrastructure;
+using Xunit;
+
+namespace GtMotive.Estimate.Microservice.InfrastructureTests.Specs
+{
+    [CollectionDefinition(nameof(TestCollections))]
+    public class RentalsTests(GenericInfrastructureTestServerFixture genericInfrastructureTestServerFixture)
+        : IClassFixture<GenericInfrastructureTestServerFixture>
+    {
+        [Fact]
+        public async Task RentalShouldThrowExceptionWhenCreatingWithNonValidPayload()
+        {
+            var client = genericInfrastructureTestServerFixture.Server.CreateClient();
+
+            using var emptyPayload = new StringContent("{}");
+            using var content = new StringContent(
+                JsonSerializer.Serialize(emptyPayload),
+                Encoding.UTF8,
+                "application/json");
+
+            await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+                await client.PostAsync(new Uri("api/Rentals", UriKind.Relative), content));
+        }
+    }
+}
